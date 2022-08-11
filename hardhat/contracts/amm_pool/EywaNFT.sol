4eebb8d3ede3af00c524002d2845f216796575ad
Vassili Kiritsenko <vassili.kiritsenko@gmail.com>
2022-08-11 23:27:27 +0300

    upd

--- hardhat/contracts/amm_pool/EywaNFT.sol
+++ hardhat/contracts/amm_pool/EywaNFT.sol
@@ -137,6 +137,12 @@ contract EywaNFT is ERC721Enumerable, Ownable, ERC721Burnable {
         saleActive = false;
     }
 
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString(), ".json")) : '';
+    }
+
     function getTokenStatus(uint256 tokenId) external view returns (uint8) {
         return tokenStatus[tokenId];
     }
